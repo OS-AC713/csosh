a04de6408ac133dee81700935f0f11a0f052f8ca
OS_AC713 <286123419+OS-AC713@users.noreply.github.com>
2026-08-26 16:13:35 +0400

    Update csosh.c

--- csosh.c
+++ csosh.c
@@ -1,10 +1,11 @@
+
 int scosh_st() {
     while (1) {
         __builtin_printf("[username@hostname]<>& ");
         char input[100];
 
         char wait_enter;
-        __builtin_scanf("%99s", &wait_enter); 
+        __builtin_scanf("%99s", &wait_enter, &input ); 
     }
     return 0;
 }
